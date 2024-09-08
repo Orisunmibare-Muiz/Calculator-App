2e66ca92c8cee7ca9bd3bfe469dcc7272d2fb759
Orisunmibare Muiz <orismuiz2003@gmail.com>
2024-09-08 20:27:57 +0100

    Modified

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-let screen = document.querySelector('.display');
-let inputs = document.querySelectorAll('.btn');
-let clear = document.querySelector('.btn-clear');
-let slice = document.querySelector('.btn-slice');
-let equal = document.querySelector('.btn-equal');
+// let screen = document.querySelector('.display');
+// let inputs = document.querySelectorAll('.btn');
+// let clear = document.querySelector('.btn-clear');
+// let slice = document.querySelector('.btn-slice');
+// let equal = document.querySelector('.btn-equal');
 
-inputs.forEach(input => {
-    input.addEventListener('click', (e) => {
-        let value = e.target.value;
-        screen.value += value;
-    })
-})
+// inputs.forEach(input => {
+//     input.addEventListener('click', (e) => {
+//         let value = e.target.value;
+//         screen.value += value;
+//     })
+// })
 
-clear.addEventListener('click', ()=> {
-    screen.value = "";
-})
+// clear.addEventListener('click', ()=> {
+//     screen.value = "";
+// })
 
-equal.addEventListener('click', ()=> {
-    let answer = eval(screen.value);
-    if(screen.value === "") {
-        screen.value = ""
-    } else {
-        screen.value = answer;
-    }
-})
+// equal.addEventListener('click', ()=> {
+//     let answer = eval(screen.value);
+//     if(screen.value === "") {
+//         screen.value = ""
+//     } else {
+//         screen.value = answer;
+//     }
+// })
 
-slice.addEventListener('click', ()=> {
-    screen.value = screen.value.slice(0,-1);
-})
+// slice.addEventListener('click', ()=> {
+//     screen.value = screen.value.slice(0,-1);
+// })
 
