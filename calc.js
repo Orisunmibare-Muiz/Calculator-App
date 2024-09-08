01ae9bc853253c350b1c4e3e4b251b4c9e425d59
Orisunmibare Muiz <orismuiz2003@gmail.com>
2024-09-08 19:41:33 +0100

    files added

---
+++ calc.js
@@ -0,0 +1,30 @@
+let screen = document.querySelector('.display');
+let inputs = document.querySelectorAll('.btn');
+let clear = document.querySelector('.btn-clear');
+let slice = document.querySelector('.btn-slice');
+let equal = document.querySelector('.btn-equal');
+
+inputs.forEach(input => {
+    input.addEventListener('click', (e) => {
+        let value = e.target.value;
+        screen.value += value;
+    })
+})
+
+clear.addEventListener('click', ()=> {
+    screen.value = "";
+})
+
+equal.addEventListener('click', ()=> {
+    let answer = eval(screen.value);
+    if(screen.value === "") {
+        screen.value = ""
+    } else {
+        screen.value = answer;
+    }
+})
+
+slice.addEventListener('click', ()=> {
+    screen.value = screen.value.slice(0,-1);
+})
+
